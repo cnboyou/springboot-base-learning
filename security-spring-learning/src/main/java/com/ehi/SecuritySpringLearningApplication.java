7cdcf859015d5427ddde5719a20015158a322b20
Mr_Wang <318448121@qq.com>
2020-05-06 14:49:16 +0800

    first commita

---
+++ security-spring-learning/src/main/java/com/ehi/SecuritySpringLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySpringLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySpringLearningApplication.class, args);
+    }
+
+}
---
+++ security-spring-learning/src/test/java/com/ehi/SecuritySpringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
