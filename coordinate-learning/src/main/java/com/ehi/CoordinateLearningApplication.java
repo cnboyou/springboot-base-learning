5693ec6575f3d7f674c977dfdc20e054ac212383
Mr_Wang <318448121@qq.com>
2020-05-06 16:54:30 +0800

    first commit

---
+++ coordinate-learning/src/main/java/com/ehi/CoordinateLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinateLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoordinateLearningApplication.class, args);
+    }
+
+}
