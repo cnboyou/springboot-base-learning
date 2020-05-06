5fb978bad7beca8e8fa17166e5be79fe6f085695
Mr_Wang <318448121@qq.com>
2020-05-06 14:42:19 +0800

    first commit

---
+++ redis-learning/src/test/java/com/ehi/redislearning/RedisLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehi.redislearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
