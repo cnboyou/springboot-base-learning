1f1af4f2ad426f3bccb770c5fbf8693f7c06e739
Mr_Wang <318448121@qq.com>
2020-05-06 14:49:41 +0800

    first commita

---
+++ swagger2-learning/src/main/java/com/ehi/swagger2learning/Swagger2LearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi.swagger2learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2LearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger2LearningApplication.class, args);
+    }
+
+}
---
+++ swagger2-learning/src/test/java/com/ehi/swagger2learning/Swagger2LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehi.swagger2learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swagger2LearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
