7b2250f120e19ffa9688eff62c9d06b7fafa629f
Mr_Wang <318448121@qq.com>
2020-05-06 14:46:16 +0800

    first commita

---
+++ jdbc-template-learning/src/main/java/com/ehi/jdbctemplatelearning/JdbcTemplateLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi.jdbctemplatelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplateLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcTemplateLearningApplication.class, args);
+    }
+
+}
---
+++ jdbc-template-learning/src/test/java/com/ehi/jdbctemplatelearning/JdbcTemplateLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehi.jdbctemplatelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
