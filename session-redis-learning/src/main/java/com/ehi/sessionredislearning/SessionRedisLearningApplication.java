0020db5e19a1a8ee47284bc02be69465f9d73c47
Mr_Wang <318448121@qq.com>
2020-05-06 14:49:31 +0800

    first commita

---
+++ session-redis-learning/src/main/java/com/ehi/sessionredislearning/SessionRedisLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi.sessionredislearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionRedisLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionRedisLearningApplication.class, args);
+    }
+
+}
---
+++ session-redis-learning/src/test/java/com/ehi/sessionredislearning/SessionRedisLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehi.sessionredislearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionRedisLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
