7c1bebf2527c3f29b8d11d833b87c43c509c34ee
Mr_Wang <318448121@qq.com>
2020-05-06 17:28:11 +0800

    first commit

---
+++ message-rabbitmq-learning/src/main/java/com/ehi/MessageRabbitmqLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageRabbitmqLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageRabbitmqLearningApplication.class, args);
+    }
+
+}
