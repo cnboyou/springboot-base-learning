5143bff966ee17d11084db2c57a819a2aea22b57
Mr_Wang <318448121@qq.com>
2020-05-06 15:38:55 +0800

    first commit

---
+++ rmi-learning/src/main/java/com/ehi/RmiLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmiLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmiLearningApplication.class, args);
+    }
+
+}
