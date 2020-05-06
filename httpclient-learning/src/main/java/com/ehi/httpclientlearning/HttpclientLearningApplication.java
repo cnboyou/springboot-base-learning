568b9690f6dbca0b52171a0285a14f6cb7007482
Mr_Wang <318448121@qq.com>
2020-05-06 15:05:33 +0800

    first commit

---
+++ httpclient-learning/src/main/java/com/ehi/httpclientlearning/HttpclientLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi.httpclientlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpclientLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpclientLearningApplication.class, args);
+    }
+
+}
---
+++ httpclient-learning/src/test/java/com/ehi/httpclientlearning/HttpclientLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehi.httpclientlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpclientLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
