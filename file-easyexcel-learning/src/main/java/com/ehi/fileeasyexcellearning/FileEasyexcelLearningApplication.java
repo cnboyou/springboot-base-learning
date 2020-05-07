38e9cee29ec5f14fbd5759ca9d41dc0a6a5224b2
Mr_Wang <318448121@qq.com>
2020-05-07 17:04:36 +0800

    first commit

---
+++ file-easyexcel-learning/src/main/java/com/ehi/fileeasyexcellearning/FileEasyexcelLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi.fileeasyexcellearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileEasyexcelLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileEasyexcelLearningApplication.class, args);
+    }
+
+}
---
+++ file-easyexcel-learning/src/test/java/com/ehi/fileeasyexcellearning/FileEasyexcelLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehi.fileeasyexcellearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileEasyexcelLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
