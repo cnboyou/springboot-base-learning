3656c1a41783c96a350f1a33d68088c61043c733
Mr_Wang <318448121@qq.com>
2020-05-08 10:59:16 +0800

    easyexcel

---
+++ feign-server-learning/src/main/java/com/ehi/FeignServerLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignServerLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignServerLearningApplication.class, args);
+    }
+
+}
