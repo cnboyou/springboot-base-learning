d73a5c1d878b3271df8392a5a89786e30a4923e0
Mr_Wang <318448121@qq.com>
2020-05-08 14:35:21 +0800

    redis

--- redis-learning/src/main/java/com/ehi/service/HelloService.java
+++ redis-learning/src/main/java/com/ehi/service/HelloService.java
@@ -2,6 +2,9 @@ package com.ehi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,11 +18,28 @@ import org.springframework.stereotype.Service;
 public class HelloService {
     @Autowired
     RedisTemplate redisTemplate;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
 
     public void hello() {
         redisTemplate.opsForValue().set("k1","v1");
 
         Object k1 = redisTemplate.opsForValue().get("k1");
+        System.out.println(k1);
+    }
+
+    public void hello1() {
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        ValueOperations ops = redisTemplate.opsForValue();
+        ops.set("k2","v2");
+        Object k1 = ops.get("k2");
+        System.out.println(k1);
+    }
+
+    public void hello3() {
+        ValueOperations ops = stringRedisTemplate.opsForValue();
+        ops.set("k3","v3");
+        Object k1 = ops.get("k3");
         System.out.println(k1);
     }
 }
