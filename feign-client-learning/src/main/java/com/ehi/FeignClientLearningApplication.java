d38c9241496f79eb6beccd54c4fc484d4950dd88
Mr_Wang <318448121@qq.com>
2020-05-08 10:58:44 +0800

    easyexcel

---
+++ feign-client-learning/src/main/java/com/ehi/FeignClientLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignClientLearningApplication.class, args);
+    }
+
+}
