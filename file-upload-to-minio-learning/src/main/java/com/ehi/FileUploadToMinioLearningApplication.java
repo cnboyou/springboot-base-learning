1cf3e2d6260995823a620366afb8e1cbd3f8221d
Mr_Wang <318448121@qq.com>
2020-05-11 11:57:21 +0800

    minIO

---
+++ file-upload-to-minio-learning/src/main/java/com/ehi/FileUploadToMinioLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadToMinioLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadToMinioLearningApplication.class, args);
+    }
+
+}
