56fc2267e1b1245046c7e605998c3a0f6bee520f
Mr_Wang <318448121@qq.com>
2020-05-11 16:43:31 +0800

    rocketmq

---
+++ massege-rocketmq-learning/src/main/java/com/ehi/MassegeRocketmqLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassegeRocketmqLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MassegeRocketmqLearningApplication.class, args);
+    }
+
+}
