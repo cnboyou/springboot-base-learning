c2ac1c8d8f70192496cd198acfbed4a81fd62630
Mr_Wang <318448121@qq.com>
2020-05-11 17:02:49 +0800

    message

---
+++ message-zeromq-learning/src/main/java/com/ehi/MessageZeromqLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ehi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageZeromqLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageZeromqLearningApplication.class, args);
+    }
+
+}
